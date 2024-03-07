da81fcf92ce522f4d565a14246b27de1b37965c1
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2024-03-07 19:00:49 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -44,11 +44,13 @@ const list = new ListTemplate(ul)
 form.addEventListener('submit', (e: Event)=>{
     e.preventDefault();
 
+    let values : [string, string ,number] = [tofrom.value, details.value, amount.valueAsNumber];
+
     let doc: HasFomatter;
     if (type.value === 'invoice'){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
 
     list.render(doc, type.value, 'end')
